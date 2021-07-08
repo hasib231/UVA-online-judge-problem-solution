fc4a70c3e41d59194d7fd7412b8316b4fcffffaf
Hasib <67379993+hasib231@users.noreply.github.com>
2021-07-08 22:23:32 +0600

    Add files via upload

---
+++ uva 10189 solution.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int m,n,i,j;
+    while(1)
+    {
+        cin>>m,n;
+        int ara[m][n];
+        if(m==0 && n==0)
+        {
+            break;
+        }
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                cin>>ara[i][j];
+            }
+        }
+
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+
+            }
+        }
+    }
+}
