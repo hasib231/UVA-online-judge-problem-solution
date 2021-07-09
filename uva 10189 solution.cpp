fad92a74ede6e222d12fcc2dab83758b92593a70
Hasib <67379993+hasib231@users.noreply.github.com>
2021-07-09 22:18:43 +0600

    Add files via upload

--- uva 10189 solution.cpp
+++ uva 10189 solution.cpp
@@ -1,32 +1,58 @@
 #include<iostream>
+#include<cstdio>
 
 using namespace std;
 
 int main()
 {
-    int m,n,i,j;
+    int m,n,i,j,x=1;
     while(1)
     {
-        cin>>m,n;
-        int ara[m][n];
+        cin>>n>>m;
+        char ara[n][m];
         if(m==0 && n==0)
         {
             break;
         }
-        for(i=0;i<m;i++)
+        getchar();
+        for(i=0;i<n;i++)
         {
-            for(j=0;j<n;j++)
-            {
-                cin>>ara[i][j];
-            }
+                cin>>ara[i];
         }
 
-        for(i=0;i<m;i++)
+        for(i=0;i<n;i++)
         {
-            for(j=0;j<n;j++)
+            for(j=0;j<m;j++)
             {
+                if(ara[i][j]=='*')
+                {
+                    continue;
+                }
+                int k=0;
 
+                if(i+1<n && ara[i+1][j]=='*'){k++;}
+                if(i+1<n && j+1<m && ara[i+1][j+1]=='*'){k++;}
+                if(j+1<m && ara[i][j+1]=='*'){k++;}
+                if(i-1>=0 && j+1<m && ara[i-1][j+1]=='*'){k++;}
+                if(i-1>=0 && ara[i-1][j]=='*'){k++;}
+                if(i-1>=0 && j-1>=0 && ara[i-1][j-1]=='*'){k++;}
+                if(j-1>=0 && ara[i][j-1]=='*'){k++;}
+                if(i+1<n && j-1>=0 && ara[i+1][j-1]=='*'){k++;}
+
+                ara[i][j]=k+'0';
             }
+
+        }
+        cout<<"Field #"<<x<<":"<<endl;
+        x++;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                cout<<ara[i][j];
+            }
+            cout<<endl;
         }
     }
+    return 0;
 }
